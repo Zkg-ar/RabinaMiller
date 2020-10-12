4f6829f6a839cd46b96ef3b6f9c0a742da5f75c9
Zkg-ar <63973151+Zkg-ar@users.noreply.github.com>
2020-10-13 00:17:58 +0300

    Update Main.java

--- Рабина-Миллера/src/com/company/Main.java
+++ Рабина-Миллера/src/com/company/Main.java
@@ -77,66 +77,6 @@ public static boolean checkNum(int number)
     return true;
 }
 
-//Данный метод работает верно, если простые числа меньше 100;
-//    public static boolean MillerRabinTest()
-//    {
-//        int n = generateNum();
-//
-//
-//        // представим n − 1 в виде (2^s)·t, где t нечётно, это можно сделать последовательным делением n - 1 на 2
-//        int t = n - 1;
-//        int k = 5;
-//        int s = 0;
-//
-//        while (t % 2 == 0)
-//        {
-//            t /= 2;
-//            s += 1;
-//        }
-//
-//        // повторить k раз
-//        for (int i = 0; i < k; i++)
-//        {
-//            // выберем случайное целое число a в отрезке [2, n − 2]
-//            int a;
-//            do
-//            {
-//                a = (int) (Math.random() * 1000);
-//            }
-//            while (a < 2 || a >= n - 2);
-//
-//            // x ← a^t mod n, вычислим с помощью возведения в степень по модулю
-//
-//            int x = (int)Math.pow(a,t)%n;
-//
-//            // если x == 1 или x == n − 1, то перейти на следующую итерацию цикла
-//            if (x == 1 || x == n - 1)
-//                continue;
-//
-//            // повторить s − 1 раз
-//            for (int r = 1; r < s; r++)
-//            {
-//                // x ← x^2 mod n
-//
-//                x = (int)Math.pow(x,2)%n;
-//
-//                // если x == 1, то вернуть "составное"
-//                if (x == 1)
-//                    return false;
-//
-//                // если x == n − 1, то перейти на следующую итерацию внешнего цикла
-//                if (x == n - 1)
-//                    break;
-//            }
-//
-//            if (x != n - 1)
-//                return false;
-//        }
-//
-//        // вернуть "вероятно простое"
-//        return true;
-//    }
-
     public static void main(String[] args) {
         mas = primeNum(10000);
 //        System.out.println(MillerRabinTest());
